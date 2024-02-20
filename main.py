9ce0ef76acf8ea42ed7c84ec4b41426d61fea05b
PovilasKorop <povilask007@gmail.com>
2024-02-20 07:34:34 +0200

    Updates for better way of showing results

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import nltk_spacy
 import os
 import textract
+from tabulate import tabulate
 
 resume_text = str(textract.process('data/resume/resume.pdf'), 'UTF-8').lower()
 keywords_resume_spacy = nltk_spacy.spacy_keywords(resume_text)
@@ -9,8 +10,8 @@ keywords_resume_nltk = nltk_spacy.nltk_keywords(resume_text)
 print('=' * 50)
 print('RESUME KEYWORDS')
 print('=' * 50)
-print(f'{len(keywords_resume_spacy)} SpaCy keywords: {keywords_resume_spacy}, ...')
-print(f'{len(keywords_resume_nltk)} NLTK keywords: {keywords_resume_nltk}, ...')
+print(f'{len(keywords_resume_spacy)} SpaCy keywords: {keywords_resume_spacy}')
+print(f'{len(keywords_resume_nltk)} NLTK keywords: {keywords_resume_nltk}')
 print('=' * 50)
 print()
 
@@ -27,12 +28,10 @@ for file_name in sorted(os.listdir('data/jd/')):
 
 # ---------------------------
 
+similarities = []
+
 # Processing job descriptions
 for job_description in job_descriptions:
-    print('=' * 50)
-    print('Resume VS ' + job_description['filename'] + ':')
-    print('-' * 50)
-
     # Matching by Cosine Similarity
     text = [resume_text, job_description['text']]
 
@@ -41,22 +40,32 @@ for job_description in job_descriptions:
 
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(text)
-    print(f'Match based on Cosine Similarity: {cosine_similarity(count_matrix)[0][1]:.2%}')
+    cosine = cosine_similarity(count_matrix)[0][1]
 
     # Matching with SpaCy keywords
     keywords_jd = nltk_spacy.spacy_keywords(job_description['text'])
     keywords_matched_spacy = set(keywords_jd).intersection(keywords_resume_spacy)
-    matchPercentage = len(keywords_matched_spacy) / len(keywords_resume_spacy)
-
-    print(f'{len(keywords_matched_spacy)} SpaCy keywords matched ({matchPercentage:.2%}): {keywords_matched_spacy}')
+    percentage_spacy = len(keywords_matched_spacy) / len(keywords_resume_spacy)
 
     # Matching with NLTK keywords
     keywords_jd = nltk_spacy.nltk_keywords(job_description['text'])
     keywords_matched_nltk = set(keywords_jd).intersection(keywords_resume_nltk)
+    percentage_nltk = len(keywords_matched_nltk) / len(keywords_resume_nltk)
 
-    matchPercentage = len(keywords_matched_nltk) / len(keywords_resume_nltk)
+    similarities.append([
+        job_description['filename'],
+        f'{cosine:.2%}',
+        f'{percentage_spacy:.2%}',
+        # ' '.join(keywords_matched_spacy),
+        f'{percentage_nltk:.2%}',
+        # ' '.join(keywords_matched_nltk)
+    ])
 
-    print(f'{len(keywords_matched_nltk)} NLTK keywords matched ({matchPercentage:.2%}): {keywords_matched_nltk}')
-
-    print('-' * 50)
-    print()
+print(tabulate(similarities, headers=[
+    'File',
+    'Cosine %',
+    'SpaCy %',
+    # 'SpaCy Keywords',
+    'NLTK %',
+    # 'NLTK Keywords'
+]))
